9605f4fda4d0160a22353367abd53d9ac1c16c3b
pauca <paucarrio@gmail.com>
2013-01-02 17:17:02 +0100

    s

--- addProperty2SDF.scala
+++ addProperty2SDF.scala
@@ -1,3 +1,4 @@
+#
 
 import scala.io._
 import scala.collection.mutable.ArrayBuffer
@@ -6,7 +7,8 @@ import java.io.FileWriter
 object addProperty2SDF{
   def main(args: Array[String]) {
     val usage = """
-scala addProperty2SDF.scala -isdf <inputSDF> -ipropertyf <inputProperyFile> -opropertyName <NewPropertyName> -osdf <OutputSdf>
+scala addProperty2SDF.scala -isdf <inputSDF> [ -ipropertyf <inputProperyFile> | -ipropertyValue <ConstantPropertyValue>] 
+    -opropertyName <NewPropertyName> -osdf <OutputSdf>
                 """      
   /* Input parsing & processing */
     if (args.length == 0) { 
@@ -23,9 +25,10 @@ scala addProperty2SDF.scala -isdf <inputSDF> -ipropertyf <inputProperyFile> -opr
                                           sys.exit(1)
         case "-isdf"          :: value :: tail => nextOption(map ++ Map( "isdf" -> value ), tail)
         case "-osdf"          :: value :: tail => nextOption(map ++ Map( "osdf" -> value ), tail)
-        case "-ipropertyf"    :: value :: tail => nextOption(map ++ Map( "ipropertyf"    -> value ), tail)
+        case "-ipropertyf"    :: value :: tail => nextOption(map ++ Map( "ipf"    -> value ), tail)
         case "-opropertyName" :: value :: tail => nextOption(map ++ Map( "ipropertyName" -> value ), tail)
-        case option :: tail => println("Unknown option:\t"+option) 
+        case "-ipropertyValue" :: value :: tail => nextOption(map ++ Map( "ipv" -> value ), tail)
+         case option :: tail => println("Unknown option:\t"+option) 
                                println(usage) 
                                sys.exit(1) 
       }
@@ -36,26 +39,35 @@ scala addProperty2SDF.scala -isdf <inputSDF> -ipropertyf <inputProperyFile> -opr
     try{
       var ifileNameSdf      = options("isdf").toString 
       var ofileNameSdf      = options("osdf").toString  
-      var ifileNameProperty = options("ipropertyf").toString  
+      var ifileNameProperty = ""
+      try{ ifileNameProperty = options("ipf").toString }catch{  case e: java.util.NoSuchElementException => println("Warning: ipropertyf not provided")}
+      var iPropertyValue = ""
+      try{  iPropertyValue  = options("ipv").toString }catch{  case e: java.util.NoSuchElementException => println("Warning: ipropertyValue not provided ")}
       var propertyName      = options("ipropertyName").toString 
 
-     if(ifileNameSdf.size == 0 | ofileNameSdf.size == 0 | ifileNameProperty.size == 0 | propertyName.size == 0){
+     if(ifileNameSdf.size == 0 | ofileNameSdf.size == 0 | propertyName.size == 0){
         throw new Exception("Error: Missing input.")
       }
+      if(  ( ifileNameProperty.size == 0 & iPropertyValue.size == 0) | ( ifileNameProperty.size > 0 & iPropertyValue.size > 0)){
+        throw new Exception("Error: Missing input: properties.")
+      }
+      var readPropertyFile = !( ifileNameProperty.size == 0 )
+        
   /* new file creation*/
 
       var output = new java.io.FileWriter( ofileNameSdf )
       var lineBuffer = new ArrayBuffer[String]() 
       try{ 
-        var prop = io.Source.fromFile(ifileNameProperty).getLines().toList
+        var prop = List[String]()
+        if(readPropertyFile) { prop = io.Source.fromFile(ifileNameProperty).getLines().toList}
         var imol : Int = 0;
         for( line <- io.Source.fromFile(ifileNameSdf).getLines()){
           if( line.startsWith("$$$$") ){
-            lineBuffer.append( "" )
             lineBuffer.append(">  <"+ propertyName + ">")
-            lineBuffer.append( prop(imol) )
+            if(readPropertyFile){ lineBuffer.append( prop(imol) )
+            }else{ lineBuffer.append( iPropertyValue )}
             imol = imol + 1
-            lineBuffer.append( "" )
+             lineBuffer.append( "" )    
             lineBuffer.append( line )
             
             for( lineB <- lineBuffer ){
@@ -66,7 +78,7 @@ scala addProperty2SDF.scala -isdf <inputSDF> -ipropertyf <inputProperyFile> -opr
             lineBuffer.append(line)
           } 
         }
-        if(imol != prop.size ) 
+        if(readPropertyFile & ( imol != prop.size ))
           throw new Exception("Error: A mismatch between numer of properties and number of molecules.")
         println("\nDone!")
       }catch{
@@ -76,7 +88,7 @@ scala addProperty2SDF.scala -isdf <inputSDF> -ipropertyf <inputProperyFile> -opr
                 
 }
    }catch{
-      case e: java.util.NoSuchElementException    => println( "Incorrect input!\n" + usage)
+      case e: java.util.NoSuchElementException    => println( "Incorrect input!\n"+ e + usage)
       case e: Exception                           => println( "Exception caught:\n" + e);
    }
   }
